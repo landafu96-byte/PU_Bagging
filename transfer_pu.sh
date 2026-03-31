ab385cbd7e6a7960e76b79863b09c30d8b484644
landafu96-byte <landafu96@gmail.com>
2026-03-31 11:20:12 +0800

    Update transfer_pu.sh

--- transfer_pu.sh
+++ transfer_pu.sh
@@ -6,7 +6,7 @@
 train_data_dir="/base/data/train"
 test_data_dir="/base/data/test"
 model="/base/checkpoint.pth.tar"
-org_model_path="/base/model/3D"
+origin_model_path="/base/model/3D"
 new_model_path="/base/model/2D"
 result_path="/base/result"
 
@@ -17,7 +17,7 @@ conda activate cgcnn
 for i in {1..100}
 do
 python train_test_split.py
-python transform.py $train_data_dir --model_path "$org_model_path/$i.pth.tar" --task classification --epochs 45  --wd 0.0005 --lr 0.002  --momentum 0.95 -b 164  \
+python transform.py $train_data_dir --model_path "$origin_model_path/$i.pth.tar" --task classification --epochs 45  --wd 0.0005 --lr 0.002  --momentum 0.95 -b 164  \
  --atom-fea-len 64  --n-conv 3  --n-h 1 --h-fea-len 128 --lr-milestones 30 > log
 cp checkpoint.pth.tar "$new_model_path/$i.pth.tar"
 
