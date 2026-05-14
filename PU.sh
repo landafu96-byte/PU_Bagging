7fc7effd4041bc2c451388c5aac62471974debb2
landafu96-byte <landafu96@gmail.com>
2026-05-14 15:43:36 +0800

    Update PU.sh

--- PU.sh
+++ PU.sh
@@ -4,7 +4,6 @@
 #PBS -l walltime=720:00:00
 
 train_data_dir="/base/data/train"
-test_data_dir="/base/data/test"
 model="/base/checkpoint.pth.tar"
 model_path="/base/model/3D"
 result_path="/base/result"
@@ -19,7 +18,4 @@ python train_test_split.py
 python main.py $train_data_dir --task classification --epochs 40  --wd 0.0005 --lr 0.001  --momentum 0.95 -b 98  \
  --atom-fea-len 64  --n-conv 3  --n-h 1 --h-fea-len 128  > log
 cp checkpoint.pth.tar "$model_path/$i.pth.tar"
-
-python predict.py $model $test_data_dir >log1
-cp test_results.csv "$result_path/test_results_$i.csv"
 done
